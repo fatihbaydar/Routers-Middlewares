312a2c43fbd74ceafc401bcea69eb5a63de0954e
fatihbaydar <fatihbaydar2004@gmail.com>
2024-11-24 13:50:18 +0300

    router

--- router.js
+++ router.js
@@ -10,7 +10,33 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
 /* ------------------------------------------------------- */
-//? "Router" is special app for URL control in ExpressJS.
+//? "Router" ExpressJS te URL kontrolü için özel bir uygulamadır.
+
+// app.get("/welcome", (req,res)=> {
+//     res.send({
+//         message:"Welcome"
+//     })
+// })
+
+//* express.Router()
+const router = express.Router()
+
+// router.get("/", (req,res) =>{res.send({message:"get isteği"})} )
+// router.post("/", (req,res) =>{res.send({message:"post isteği"})} )
+// router.put("/", (req,res) =>{res.send({message:"put isteği"})} )
+// router.delete("/", (req,res) =>{res.send({message:"delete isteği"})} )
+// router.all("/", (req,res) =>{res.send({message:"all da yapabilir"})} )
+
+router.route("/")
+.get((req,res) =>{res.send({message:"get isteği"})} )
+.post((req,res) =>{res.send({message:"post isteği"})} )
+.put((req,res) =>{res.send({message:"put isteği"})} )
+.delete((req,res) =>{res.send({message:"delete isteği"})} )
+.all((req,res) =>{res.send({message:"all da yapabilir"})} )
+
+
+app.use(router) // app'e router kullanıldığını haberdar etmek gerek. bu isteklerin sonunda olabileceği gibi başında da olabilir.
+
 
 
 /* ------------------------------------------------------- */
