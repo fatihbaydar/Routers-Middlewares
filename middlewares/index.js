d0d26079ce6dacc92b0363baac14e08db28aa69c
fatihbaydar <fatihbaydar2004@gmail.com>
2024-11-24 18:23:15 +0300

    export with function

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,21 +3,31 @@
     EXPRESSJS - MIDDLEWARES
 ------------------------------------------------------- */
 
+// const middleFunc1 = (req, res, next) => {
+//   console.log("middleFunc1 çalıştı");
+//   req.message1 = "middleFunc1 çalıştı";
+//   next();
+// };
 
-const middleFunc1 = (req, res, next) => {
+// const middleFunc2 = (req, res, next) => {
+//   console.log("middleFunc2 çalıştı");
+//   req.message2 = "middleFunc2 çalıştı";
+//   next();
+// };
+
+// module.exports = [middleFunc1,middleFunc2]
+// module.exports = {middleFunc1,middleFunc2}
+
+/* ------------------------------------------------------- */
+
+module.exports.middleFunc1 = (req, res, next) => {
   console.log("middleFunc1 çalıştı");
   req.message1 = "middleFunc1 çalıştı";
   next();
 };
 
-const middleFunc2 = (req, res, next) => {
+module.exports.middleFunc2 = (req, res, next) => {
   console.log("middleFunc2 çalıştı");
-  req.message2 = "middleFunc2 çalıştı";
+  req.message1 = "middleFunc2 çalıştı";
   next();
 };
-
-
-// module.exports = [middleFunc1,middleFunc2]
-module.exports = {middleFunc1,middleFunc2}
-
-/* ------------------------------------------------------- */
