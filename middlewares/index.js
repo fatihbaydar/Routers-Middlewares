348b4e87dbca8ed2706aa9e0741483eb05de0d36
fatihbaydar <fatihbaydar2004@gmail.com>
2024-11-24 18:28:02 +0300

    export function within an object

--- middlewares/index.js
+++ middlewares/index.js
@@ -20,14 +20,30 @@
 
 /* ------------------------------------------------------- */
 
-module.exports.middleFunc1 = (req, res, next) => {
-  console.log("middleFunc1 çalıştı");
-  req.message1 = "middleFunc1 çalıştı";
-  next();
-};
+// module.exports.middleFunc1 = (req, res, next) => {
+//   console.log("middleFunc1 çalıştı");
+//   req.message1 = "middleFunc1 çalıştı";
+//   next();
+// };
 
-module.exports.middleFunc2 = (req, res, next) => {
-  console.log("middleFunc2 çalıştı");
-  req.message1 = "middleFunc2 çalıştı";
-  next();
+// module.exports.middleFunc2 = (req, res, next) => {
+//   console.log("middleFunc2 çalıştı");
+//   req.message1 = "middleFunc2 çalıştı";
+//   next();
+// };
+
+/* ------------------------------------------------------- */
+
+module.exports = {
+  middleFunc1: (req, res, next) => {
+    console.log("middleFunc1 çalıştı");
+    req.message1 = "middleFunc1 çalıştı";
+    next();
+  },
+
+  middleFunc2: (req, res, next) => {
+    console.log("middleFunc2 çalıştı");
+    req.message2 = "middleFunc2 çalıştı";
+    next();
+  },
 };
